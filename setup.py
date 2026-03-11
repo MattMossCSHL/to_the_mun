146c5c31c9e4b559f8871f21cc9b0b4d774fbbb7
MattMossCSHL <moss@cshl.edu>
2026-03-10 22:24:04 -0400

    added setup.py doc

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="to_the_mun",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+    ],
+)
